74dc02add02e11acb63fd8410f0d453930c7c482
tiennvn0 <tiennvn0@gmailc.om>
2020-08-25 22:28:15 +0700

    feed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import Sidebar from "./Sidebar";
 // import logo from './logo.svg';
 import "./App.css";
+import Feed from "./Feed";
+import Widgets from "./Widgets";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="app">
+      {/* <h1>Hi</h1> */}
       {/* Sidebar */}
       <Sidebar />
       {/* Feed */}
+      <Feed />
       {/* Widgets */}
+      <Widgets />
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div>
+      {/* Header */}
+      <div className="feed">
+        <div className="feed__header">
+          <h2>Home</h2>
+        </div>
+      </div>
+      {/* TweetBox */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Feed;
